65e62905ae055fcab1b4c607d3a8c28ee3e15549
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-05-08 11:35:15 +0900

    initial commit

---
+++ src/main/java/com/develop/gymmpas/GymmpasApplication.java
@@ -0,0 +1,13 @@
+package com.develop.gymmpas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymmpasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymmpasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develop/gymmpas/GymmpasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.gymmpas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymmpasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
